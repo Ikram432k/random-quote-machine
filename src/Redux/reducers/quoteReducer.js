49fdd3da3efb05e1704c4d1436279b9e89d5cb0b
ikram <ikram@animaker.com>
2022-12-26 19:15:26 +0530

    added reducer

--- src/Redux/reducers/quoteReducer.js
+++ src/Redux/reducers/quoteReducer.js
@@ -11,6 +11,8 @@ export const quoteReducer =(state = initialState, {type,payload})=>{
         return { ...state,author: payload };
         case ActionTypes.SET_QUOTE:
         return {...state,quote: payload };
+        case ActionTypes.REMOVE_OLDQUOTE:
+        return {...state,author:"",quote: ""};
         default:
         return state;
     }
