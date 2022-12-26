1501bcfd10ffa5b383fcf3a39b8088a7288fc8a5
ikram <ikram@animaker.com>
2022-12-26 18:42:11 +0530

    created header component

---
+++ src/Components/head/header.js
@@ -0,0 +1,11 @@
+import './header.scss';
+const Head =()=>{
+return(
+    <div>
+        <header className="headDiv">
+            <h1>Today's Random Quote</h1>
+        </header>
+    </div>
+)
+}
+export default Head; 
