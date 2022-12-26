a6ad1c13d8a753c393fd604acd4f4dc99099f101
ikram <ikram@animaker.com>
2022-12-26 18:41:26 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Head from './Components/head/header';
+import Quote from './Components/quote/quote'
+import './App.scss';
 function App() {
   return (
     <div className="App">
+      <Head/>
+      <Quote/>
     </div>
   );
 }
