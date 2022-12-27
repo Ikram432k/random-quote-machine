46d771609e001a01a3499396a89c69f23a6a691b
ikram <ikram@animaker.com>
2022-12-27 20:55:33 +0530

    quote.js updated

--- src/Components/quote/quote.js
+++ src/Components/quote/quote.js
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useLayoutEffect,useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { set_quote,set_author,remove_oldquote } from "../../Redux/actions/todaysQuote";
 import './quote.scss'
+import { gsap } from "gsap";
+
 const Quote =()=>{
+    const topDiv = useRef();
+    const btnDiv = useRef();
     const dispatch = useDispatch();
     const storeData = useSelector((state)=> state.allquotes);
     const { author,quote } = storeData;
@@ -14,7 +18,17 @@ const Quote =()=>{
     //     quotes:"",
     //     author:""
     // });
-
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+          // // use scoped selectors
+          // gsap.to(".box", { rotation: 360 });
+          // or refs
+          gsap.to(btnDiv.current, { rotation: 360 });
+          
+        }, topDiv);
+        
+        return () => ctx.revert();
+      }, []);
     const getQuote =async()=>{
         try{
             const response = await fetch("https://api.quotable.io/random");
@@ -28,14 +42,21 @@ const Quote =()=>{
         }
        
     }
+    const onEnter = ({ currentTarget }) => {
+        gsap.to(currentTarget, { scale: 1.2 });
+      };
+      
+      const onLeave = ({ currentTarget }) => {
+        gsap.to(currentTarget, { scale: 1 });
+      };
     return(
-        <div className="q-maindiv">
+        <div className="q-maindiv"  onMouseEnter={onEnter} onMouseLeave={onLeave}>
             <div className="q-div">
                 <p className="q-p">"{quote}"</p>
                 <p className="q-a">- {author}</p>
             </div>
-            <div className="q-btn">
-                <button onClick={getQuote}><i className="fa fa-refresh"></i></button>
+            <div ref={topDiv} className="q-btn">
+                <button  onClick={getQuote }><i ref={btnDiv} className="fa fa-refresh"></i></button>
             </div>
         </div>
     )
