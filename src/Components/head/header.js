f56206f5bbc1e431d7a949b7581591fc7a97aa80
ikram <ikram@animaker.com>
2022-12-27 20:55:14 +0530

    header.js updated

--- src/Components/head/header.js
+++ src/Components/head/header.js
@@ -1,9 +1,39 @@
 import './header.scss';
+import { gsap } from "gsap";
+import { useRef,useLayoutEffect,useState, useEffect} from 'react';
 const Head =()=>{
+    const topDiv = useRef();
+    const headDiv = useRef();
+    const [count, setCount] = useState(0);
+    const [delayedCount, setDelayedCount] = useState(0);
+    useLayoutEffect(() => {
+      let ctx = gsap.context(() => {
+        // // use scoped selectors
+        // gsap.to(".box", { rotation: 360 });
+        // or refs
+        gsap.to(".headDiv", {
+            x: 100,
+            repeat: -1,
+            repeatDelay: 1,
+            yoyo: true
+          });
+        gsap.to(headDiv.current, { rotation: 360 });
+        
+      }, topDiv);
+      
+      return () => ctx.revert();
+    }, [delayedCount]);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setDelayedCount(count);
+        }, 1000);
+    
+        return () => clearTimeout(timer);
+      }, [count]);
 return(
-    <div>
-        <header className="headDiv">
-            <h1>Today's Random Quote</h1>
+    <div ref={topDiv} className="topDiv">
+        <header ref={headDiv} className="headDiv">
+            <h1 onClick={() => setCount(count + 1)}>Today's Random Quote</h1>
         </header>
     </div>
 )
